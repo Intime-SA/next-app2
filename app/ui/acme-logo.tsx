20d975ed6e1133e6d9caa61b23961552fe45b80b
Intime-SA <invertimesa@gmail.com>
2024-09-29 18:04:09 -0300

    cambios

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,10 +1,10 @@
 import { GlobeAltIcon } from "@heroicons/react/24/outline";
-import { luisitana } from "./fonts";
+import { lusitana } from "./fonts";
 
 export default function AcmeLogo() {
   return (
     <div
-      className={`${luisitana.className} flex flex-row items-center leading-none text-white`}
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
       <p className="text-[44px]">Acme</p>
