0a59cb80e810a3c2c1f3b692e9cc006971cabce3
Intime-SA <invertimesa@gmail.com>
2024-09-29 18:05:41 -0300

    otra

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,7 +2,7 @@ import { Inter, Lusitana, Montserrat } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
 
-export const luisitana = Lusitana({
+export const lusitana = Lusitana({
   subsets: ["latin"],
   weight: ["400", "700"],
 });
